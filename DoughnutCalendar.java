a6da81c5f25fa7c78feed34926fc5211627ed171
Pawel Drewniak <1005763d@student.gla.ac.uk>
2011-11-17 16:25:10 +0000

    Fixing hang-on-close bug

--- DoughnutCalendar.java
+++ DoughnutCalendar.java
@@ -4,6 +4,8 @@ import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
