71eac841552ecab9e3b37a72ba2c247612da0cba
Kostis <provlhma@gmail.com>
2011-11-17 15:54:08 +0000

    Add font to Context

--- Context.java
+++ Context.java
@@ -1,5 +1,8 @@
+import java.awt.Font;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+
+import javax.swing.JLabel;
 
 import calendar_ex.CalendarDate;
 
@@ -7,6 +10,7 @@ import calendar_ex.CalendarDate;
 public class Context {
 	private CalendarDate clock_date; // real world clock
 	private CalendarDate view_date; // the date we are viewing
+	private Font font;
 	
 	private static Context c;
 	
@@ -32,6 +36,10 @@ public class Context {
 		this.clock_date = current_date;
 	}
 	
+	public Font getFont() {
+		return font;
+	}
+	
 	Context() {
 		Calendar now = new GregorianCalendar();
 		int d = now.get(Calendar.DAY_OF_MONTH);
@@ -39,5 +47,11 @@ public class Context {
 		int y = now.get(Calendar.YEAR);
 		view_date = new CalendarDate(d,	m, y);
 		clock_date = new CalendarDate(d, m, y);
+		
+		// This is the default font for the calendar
+		JLabel eventLabel = new JLabel("Event");
+		font = new Font(eventLabel.getFont().getFontName(),
+				eventLabel.getFont().getStyle(), 3 * eventLabel.getFont()
+						.getSize());
 	}
 }
